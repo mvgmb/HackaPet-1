fc2b6f83f2919242edbe43ea527ad4f6e46d01eb
Italo Henrique <italo@MacBook-Air-de-Italo.local>
2018-08-10 03:13:36 -0300

    Button Submit

--- src/RightScreen/Submited.js
+++ src/RightScreen/Submited.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
 class Submited extends React.Component{
 
+    handleClick() {
+        return <div> Submeta um arquivo! </div>
+      }
+    
     render(){
         return (
-            <button> + </button>
+            <button onClick={(e) => this.handleClick(e)} > + </button>
         )
     }
 }
