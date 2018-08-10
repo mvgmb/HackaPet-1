3cb36d997b423182829ccf2746130e29e0c58d78
Italo Henrique <italo@MacBook-Air-de-Italo.local>
2018-08-10 03:01:44 -0300

    submited

---
+++ src/RightScreen/Submited.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class Submited extends React.Component{
+
+    render(){
+        return (
+            <button> + </button>
+        )
+    }
+}
+
+export default Submited;
